87ad83715cd559cc6febd5586ba5d77a603b8b3f
WibyFabian08 <wibyfabian08@gmail.com>
2020-11-17 17:23:15 -0800

    Mencoba Menambahkan File Baru

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from '../src/container/Home/Home';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Home></Home>
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
